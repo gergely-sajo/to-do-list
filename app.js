69a72dcbe94fe66b909b49a49e07ab015930a32d
gergely-sajo <sajo.gergo@gmail.com>
2020-06-07 19:26:46 +0200

    Completed styles, and modified app.js

--- app.js
+++ app.js
@@ -5,14 +5,24 @@ let myList = document.getElementById("myList")
 myForm.addEventListener("submit", (e) => {
     e.preventDefault()
     createItem(myField.value)
-    myField.value = ""
-    myField.focus()
 });
 
 function createItem(x) {
-let myHTML = `<li>${x} <button onclick="deleteItem(this)">delete</button></li>`
-myList.insertAdjacentHTML("beforeend", myHTML)
-};
+    if (x == "") {
+        alert("Write something in the field.")
+    } else {
+        let myHTML = `
+        <div class="item">
+            <li>${x}</li>
+                <button class="delete-button" onclick="deleteItem(this)">delete</button>
+        </div>`
+
+        myList.insertAdjacentHTML("beforeend", myHTML)
+    }
+    
+    myField.value = ""
+    myField.focus()
+}
 
 function deleteItem(elementToDelete) {
     elementToDelete.parentElement.remove()
